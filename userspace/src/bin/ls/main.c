a77004d9bb0cc23c7b22f5289ecaa25ed2fc8584
alexei-zebra <alexei.zebra@yandex.ru>
2026-05-03 02:58:36 +0300

    update: ls now use args.h

--- userspace/src/bin/ls/main.c
+++ userspace/src/bin/ls/main.c
@@ -5,67 +5,46 @@
 #include <ulib/syscall.h>
 #include <ulib/args.h>
 
-void main(int argc, const char **argv)
+int main(int argc, char *argv[])
 {
-    int result_a = 0;
-    int result_l = 0;
-    int result_p = 0;
-    result_a = get_opt(argc, argv, 'a', false);
-    result_l = get_opt(argc, argv, 'l', false);
-    result_p = get_opt(argc, argv, 'p', true);
+    char cwdbuf[256];
+    const char *path;
+    bool b_list = false,
+         b_all = false,
+         b_path = false;
 
-    printf("result_a: %d\n", result_a);
-    printf("result_l: %d\n", result_l);
-    printf("result_p: %d\n", result_p);
-    if (result_p == 1) {
-        printf("result_p_arg: %s\n", arg);
-        printf("result_p_arg: %s\n", argv[2]);
+    int opt;
+    while ((opt = getopt(argc, argv, "al")) != -1) {
+        switch (opt) {
+            case 'a': b_all = true; break;
+            case 'l': b_list = true; break;
+            case '?': printf("unknown/invalid option\n"); break;
+        }
     }
+
+    if (optind == argc - 1) {
+        b_path = true;
+        path = argv[optind];
+    }
+
+    if (!b_path)
+    {
+        sys_getcwd(cwdbuf, sizeof(cwdbuf));
+        path = cwdbuf;
+    }
+
+    char name[256];
+    uint32_t i = 0;
+    int64_t r;
+
+    printf("");
+    while ((r = sys_readdir(path, i, name)) != (int64_t)-1)
+    {
+        i++;
+        if (name[0] == '.' && !b_all) continue;
+        printf("%s%s%s", name, r == 1 ? "/" : "", b_list ? "\n" : "  ");
+    }
+    printf("");
 
     exit(0);
 }
-
-
-    // char cwdbuf[256];
-    // const char *path;
-    // char b_list = 0, b_all = 0, b_path = 0;
-
-    // if (argc > 1)
-    // {
-    //     for (int i = 1; i < argc; i++)
-    //     {
-    //         const char *arg = argv[i];
-    //         if (arg[0] == '-')
-    //         {
-    //             for (char *ops = (char*)arg + 1; *ops; ops++)
-    //             {
-    //                 if (*ops == 'l') b_list = 1;
-    //                 if (*ops == 'a') b_all  = 1;
-    //             }
-    //         }
-    //         else
-    //         {
-    //             b_path = 1;
-    //             path = arg;
-    //         }
-    //     }
-    // }
-
-    // if (!b_path)
-    // {
-    //     sys_getcwd(cwdbuf, sizeof(cwdbuf));
-    //     path = cwdbuf;
-    // }
-
-    // char name[256];
-    // uint32_t i = 0;
-    // int64_t r;
-
-    // printf("");
-    // while ((r = sys_readdir(path, i, name)) != (int64_t)-1)
-    // {
-    //     i++;
-    //     if (name[0] == '.' && !b_all) continue;
-    //     printf("%s%s%s", name, r == 1 ? "/" : "", b_list ? "\n" : "  ");
-    // }
-    // printf("");
