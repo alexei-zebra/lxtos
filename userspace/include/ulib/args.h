1901597acb79727623effe46d9ca2c30fca1d10d
alexei-zebra <alexei.zebra@yandex.ru>
2026-05-03 02:57:35 +0300

    added: args.h

--- userspace/include/ulib/args.h
+++ userspace/include/ulib/args.h
@@ -1,44 +1,95 @@
-#pragma once
-
-#include <stdbool.h>
+#include <libc/stdio.h>
 #include <libc/string.h>
 
-#define MAX_ARG_LEN 255
+char *optarg = NULL;
+int   optind = 1;
+int   opterr = 1;
+int   optopt = 0;
 
-char arg[MAX_ARG_LEN];
+static char *optcursor = NULL;
+static int  nextchar   = 0;
 
-int get_opt(const int argc, const char **argv, const char opt, const bool b_with_arg)
+int getopt(int argc, char * const argv[], const char *optstring)
 {
-    // set argument count
-
-    int cnt = argc;
-    for (int i = 1; i < argc; i++)
-        if (argv[i][0] == '-' && argv[i][1] == '-' && strlen(argv[i]) == 2) // argument is end separator
-                cnt = i;
-
-
-    // find option
-
-    for (int i = 1; i < cnt; i++) {
-        if (argv[i][0] == '-' && argv[i][1] != '-') // is a option
-            if (strlen(argv[i]) > 2) { // is a multi-option
-                for (char *ops = argv[i] + 1; *ops; ops++) {
-                    if (*ops == opt) { // is a matching option
-                        if (b_with_arg)
-                                return -1; // option requires an argument but it be in multi-option
-                        return 1; // option found
-                    }
-                }
-            } else if (strlen(argv[i]) == 2 && argv[i][1] == opt) { // is a single option and is a matching option
-                if (b_with_arg) // option requires an argument
-                    if (i + 1 < cnt) {
-                        int len = strlen(argv[i + 1]);
-                        strncpy(arg, argv[i + 1], len);
-                    } else {
-                        return -1; // option requires an argument but none provided
-                    }
-                return 1; // option found
-            }
+    /* Init: if optcursor == NULL, start new token */
+    if (optcursor == NULL) {
+        nextchar = 0;
     }
-    return 0; // option not found
+
+    if (optind >= argc || argv[optind] == NULL) {
+        return -1;
+    }
+
+    /* If the option cluster is exhausted or not yet set */
+    if (optcursor == NULL || *optcursor == '\0') {
+        if (strcmp(argv[optind], "--") == 0) {
+            optind++;
+            optcursor = NULL;
+            return -1;
+        }
+        if (argv[optind][0] != '-' || argv[optind][1] == '\0') {
+            optcursor = NULL;
+            return -1;
+        }
+        /* New option cluster */
+        optcursor = &argv[optind][1];
+        nextchar = 0;
+    }
+
+    optopt = optcursor[nextchar];
+    if (optopt == '\0') {
+        /* Empty cluster (should not happen) */
+        optind++;
+        optcursor = NULL;
+        return -1;
+    }
+
+    const char *pos = strchr(optstring, optopt);
+    if (pos == NULL) {
+        if (opterr)
+            printf("%s: illegal option -- %c\n", argv[0], optopt);
+        nextchar++;
+        if (optcursor[nextchar] == '\0') {
+            optind++;
+            optcursor = NULL;
+        }
+        return '?';
+    }
+
+    /* Option with argument */
+    if (pos[1] == ':') {
+        /* Check for attached argument */
+        if (optcursor[nextchar + 1] != '\0') {
+            if (opterr)
+                printf("%s: option '-%c' cannot have attached argument\n",
+                       argv[0], optopt);
+            optind++;
+            optcursor = NULL;
+            return '?';
+        }
+        /* Separate argument */
+        if (optind + 1 < argc) {
+            optarg = argv[++optind];
+            optind++;                   /* Move to the token after the argument */
+            optcursor = NULL;
+            return optopt;
+        } else {
+            if (opterr)
+                printf("%s: option requires an argument -- %c\n",
+                       argv[0], optopt);
+            optind++;
+            optcursor = NULL;
+            return '?';
+        }
+    } else {
+        /* Option without argument */
+        nextchar++;
+        if (optcursor[nextchar] == '\0') {
+            /* Cluster finished */
+            optind++;
+            optcursor = NULL;
+        }
+        optarg = NULL;
+        return optopt;
+    }
 }
